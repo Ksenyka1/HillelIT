cf70c1c8ff1634cc14d68ca16294f3cbca24557d
Oksana <oksanarevakina1@gmail.com>
2025-07-17 17:12:24 +0200

    Домашнє завдання

---
+++ jaBasics1/task3.js
@@ -0,0 +1,15 @@
+const number = 5;
+
+// for
+console.log("Таблиця множення (for):");
+for (let i = 1; i <= 10; i++) {
+  console.log(`${number} x ${i} = ${number * i}`);
+}
+
+// while
+console.log("Таблиця множення (while):");
+let j = 1;
+while (j <= 10) {
+  console.log(`${number} x ${j} = ${number * j}`);
+  j++;
+}
